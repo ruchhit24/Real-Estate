ca1d040b7645edb0d5eb5d079346d596a9c3e62a
ruchhit24 <ruchit1234.forfirm@gmail.com>
2024-05-21 20:41:21 +0530

    added pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
  import React from 'react'
+ import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
  
  const App = () => {
    return (
-     <div className='text-red-600 text-center'>App</div>
+     <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/about' element={<About/>}/>
+
+      </Routes>
+     </BrowserRouter>
    )
  }
  
